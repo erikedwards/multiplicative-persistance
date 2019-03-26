3ee1964b995f611d6d3d68c854ba4b23c5d0e815
erikedwards <erik.p.edwards@gmail.com>
2019-03-26 00:20:44 -0700

    works but no step counter.

---
+++ m_pers.py
@@ -0,0 +1,26 @@
+
+# get a number. multiply each digit together. repeat with the product.
+
+# counter = 1
+
+
+def m_pers(n):
+    x = 1
+    for i in range(len(str(n))):
+        x *= int(str(n)[i-1])
+    print(str(x))
+    if len(str(x)) > 1:
+        # counter += 1
+        m_pers(x)
+
+
+val = 1
+
+while 1:
+    val = input("enter number")
+    print("starting conprutations")
+    print(str(val))
+    steps = 1
+    m_pers(val)
+    # print(str(counter) + " steps")
+    val = input("1 to continue, 0 to quit")
