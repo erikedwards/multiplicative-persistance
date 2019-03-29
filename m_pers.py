74d96687f107508af9593678ff5399ee2a6b0e59
erik <erik.p.edwards@gmail.com>
2019-03-29 01:55:40 -0700

    now with step counter

--- m_pers.py
+++ m_pers.py
@@ -4,23 +4,24 @@
 # counter = 1
 
 
-def m_pers(n):
+def m_pers(n, step):
     x = 1
     for i in range(len(str(n))):
         x *= int(str(n)[i-1])
-    print(str(x))
+    print(str(step) + ": " + str(x))
     if len(str(x)) > 1:
         # counter += 1
-        m_pers(x)
+        m_pers(x, step + 1)
+    else:
+        print("Total Steps = " + str(step))
 
 
 val = 1
-
 while int(val)>0:
     val = input("enter number")
     print("starting conprutations")
-    print(str(val))
-    steps = 1
-    m_pers(val)
-    # print(str(counter) + " steps")
+    print("0: " + str(val))
+    m_pers(val, 1)
     val = input("1 to continue, 0 to quit")
+
+
